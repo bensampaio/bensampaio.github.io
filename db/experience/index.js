6449f9f2b08d4c6d3798f0952f55a120db8e03f7
Bruno Sampaio <bens.sampaio@gmail.com>
2020-01-05 17:04:56 +0100

    Update CV

--- db/experience/index.js
+++ db/experience/index.js
@@ -4,29 +4,26 @@ import safiraLogo from './safira.jpg';
 
 export default [
     {
-        "title": "Front End Developer",
+        "title": "Frontend Developer",
         "institution": { "url": "http://www.karify.com/", "logo": karifyLogo, "name": "Karify" },
         "period": { "from": { "month": 5, "year": 2016 }, "to": null },
         "summary": `
-            * ​Designed and implemented a new front-end architecture for the company product (JavaScript (ES 2015+), React, Redux, SASS, CSS Modules, Jasmine, Webpack, Karma, NodeJS, ESLint, Stylelint, npm);
-            * In parallel, together with a designer, redesigned also several sections of the current front-end (JavaScript (ES5), jQuery, Less);
-            * Participated in decision-making processes related to new features and the rethinking of already existing features;
-            * Code reviewing;
-            * Knowledge sharing via presentations related to front-end topics or the new architecture and framework I was building.
+            I was given the task of rebuilding the existing web frontend using React. This involved a lot of decision-making with stakeholders, building a design system together with a designer and defining a code style guide and quality assurance standards. In the meantime, I helped with the hiring of new developers and supervised a React Native project meant to replace the existing mobile apps.
+
+            Technologies: JavaScript (ES 2015+), React, Redux, SASS, CSS Modules, Jest, Webpack, NodeJS (express), ESLint, Stylelint, yarn workspaces.
         `,
     },
     {
-        "title": "Front End Developer",
+        "title": "Frontend Developer",
         "institution": { "url": "http://www.campusinc.nl/", "logo": campusincLogo, "name": "CampusInc" },
         "period": { "from": { "month": 12, "year": 2014 }, "to": { "month": 4, "year": 2016 } },
         "summary": `
-            * Designed the front end architecture of the product (social network for schools and universities to help bringing students and companies together​), and implemented UI components based on user experience best practices;
+            The company product was a social network that tried to fill the gap between the educational and the professional worlds.
+            * Designed the frontend architecture of the product and built its UI components based on user experience best practices;
             * Participated in functional and technical analysis, including decision-making support concerning new features to be added to the product;
-            * Migrated the entire front end solution from ECMAScript 5 to 2015;
-            * Wrote unit tests for all front end modules;
-            * In the last 8 month I was also responsible for the planning of the product.
+            * Migrated the frontend from ECMAScript 5 to 2015.
             
-            Technologies used: JavaScript (ECMAScript 2015), AngularJS, jQuery, Jasmine, Twitter Bootstrap, Webpack, NodeJS, SASS, CSS3, HTML5, Bower, Gulp, Karma, Laravel, MySQL.
+            Technologies: JavaScript (ECMAScript 2015), AngularJS, jQuery, Jasmine, Twitter Bootstrap, Webpack, NodeJS, SASS, CSS3, HTML5, Bower, Gulp, Karma, Laravel, MySQL.
         `,
     },
     {
@@ -35,16 +32,17 @@ export default [
         "period": { "from": { "month": 4, "year": 2013 }, "to": { "month": 12, "year": 2014 } },
         "summary": `
             Developed new IT solutions for Banif (a Portuguese bank) aiming to ease the bank credit workflow process.
-            * Analyzed client methodologies and requirements to find the most appropriate solutions for their working model.
-            * Designed and implemented several UI components based on user experience best practices.
-            * Established the communication between different systems using exposed Web Services.
-            * Participated on the database architecture design and implementation.
+            * Analyzed client methodologies and requirements to find the most appropriate solutions for their working model;
+            * Designed and implemented several UI components based on user experience best practices;
+            * Established the communication between different systems using exposed Web Services;
+            * Participated on the database architecture design and implementation;
             * Provided technical assistance to users.
             After one year I was promoted to Expert Software Developer and the project continued, mainly in terms of bug fixing, change requests implementation, and technical assistance.
-            * Participated in functional and technical analysis, including decision-making support concerning new features to be added to the product.
-            * Reviewed code following the industry best practices for the used technologies on the project.
+            * Participated in functional and technical analysis, including decision-making support concerning new features to be added to the product;
+            * Reviewed code following the industry best practices for the used technologies on the project;
             * Managed the deployment life-cycle in the development staging and production environment.
-            Technologies used: IBM BPM 8.0.1, JavaScript (Client and Server side), jQuery, Bootstrap, HTML, CSS, XML, Java, and SQL Server.
+            
+            Technologies: IBM BPM 8.0.1, JavaScript (Client and Server side), jQuery, Bootstrap, HTML, CSS, XML, Java, and SQL Server.
         `,
     },
     {
@@ -53,7 +51,7 @@ export default [
         "period": { "from": { "month": 6, "year": 2012 }, "to": { "month": 10, "year": 2012 } },
         "summary": `
             Developed the entire website and was responsible for all architectural decisions. In terms of UI and UX worked along side with a designer.
-            Technologies used: JavaScript, jQuery, HTML5, CSS3, PHP, CakePHP, and MySQL.
+            Technologies: JavaScript, jQuery, HTML5, CSS3, PHP, CakePHP, and MySQL.
         `,
     }
 ];
