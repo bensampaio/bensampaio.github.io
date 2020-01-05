bf374aec2bede88a955011dfaa6890b9a7d8b40b
Bruno Sampaio <bens.sampaio@gmail.com>
2020-01-05 17:24:40 +0100

    Update CV (#15)
    * Upgrade dependencies

--- db/education/index.js
+++ db/education/index.js
@@ -7,7 +7,12 @@ export default [
         "field": "Computer Science Engineering",
         "institution": { "url": "http://www.fct.unl.pt/", "logo": unlLogo, "name": "Universidade Nova de Lisboa" },
         "period": { "from": { "year": 2010 }, "to": { "year": 2013 } },
-        "summary": ""
+        "summary": `
+            Master thesis:
+            * Investigated the used of games in education and the effectiveness of collaboration as a teaching method;
+            * Built a web platform for the development, distribution and execution of collaborative educational games with singleplayer and multiplayer mode. Technologies: JavaScript, jQuery, KineticJS, JSON, HTML5, CSS3, PHP, CakePHP, NodeJS, XML, XML Schema, MySQL;
+            * Built several plugins for Moodle that provide new ways of interaction and collaboration between teachers and students, new awareness indicators, and new forms of integration with external systems. My work can be found on https://moodle.org/plugins/browse.php?list=set&id=31 and on my Github. Technologies: JavaScript, JSON, YUI, HTML5, CSS3, PHP, XML, MySQL.
+        `
     },
     {
         "degree": "Master",
