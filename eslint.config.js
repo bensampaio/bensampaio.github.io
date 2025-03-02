f19d531ba9face7588f8b7fbf6dea45ffd4aedbb
Bruno Nunes Sampaio <b.nunes.sampaio@catawiki.nl>
2025-03-02 19:46:38 +0000

    Upgrade ESLint

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import globals from 'globals';
+import { fixupConfigRules } from '@eslint/compat';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import tsParser from '@typescript-eslint/parser';
+
+const f = fileURLToPath(import.meta.url);
+const d = path.dirname(f);
+const compat = new FlatCompat({
+    baseDirectory: d,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    ...fixupConfigRules(
+        compat.extends(
+            'eslint:recommended',
+            'plugin:react/recommended',
+            'plugin:@typescript-eslint/recommended',
+            'prettier'
+        )
+    ),
+    {
+        ignores: ['.yarn', 'db', 'node_modules', 'public'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+            parser: tsParser,
+            ecmaVersion: 2024,
+            sourceType: 'module',
+        },
+
+        rules: {
+            'linebreak-style': ['error', 'unix'],
+            quotes: ['error', 'single'],
+            'react/display-name': 0,
+            'react/react-in-jsx-scope': 0,
+            'react/prop-types': 0,
+            semi: ['error', 'always'],
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+];
